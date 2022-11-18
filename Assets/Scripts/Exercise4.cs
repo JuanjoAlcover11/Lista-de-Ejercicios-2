a5fe5c1129d8b62223d7cc139f3f262be4075e3d
JuanjoAlcover11 <61268085+JuanjoAlcover11@users.noreply.github.com>
2022-11-18 10:08:45 +0100

    4

--- Assets/Scripts/Exercise4.cs
+++ Assets/Scripts/Exercise4.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Exercise4 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int correctNum;
+    private int incorrectNum;
 
-    // Update is called once per frame
-    void Update()
+    public TextMeshProUGUI correctText;
+    public TextMeshProUGUI incorrectText;
+
+    private int turnCounter = 10;
+    private int xNum;
+    private int randomNum;
+    public GameObject[] randomGuess;
+
+
+    public void Turns()
     {
-        
+        if(turnCounter > 0)
+        {
+            randomGuess[randomNum].SetActive(false);
+            turnCounter--;
+            //StartCoroutine(Options());
+        }
     }
+    
+   /* private IEnumerator Options()
+    {
+        randomNum = Random.Range(0, 2);
+        randomGuess[randomNum].SetActive(true);
+
+        if (randomNum == xNum)
+        {
+            correctNum++;
+        }
+        else
+        {
+            incorrectNum--;
+        }
+        if (turnCounter == 0)
+        {
+            randomGuess[randomNum].SetActive(false);
+            correctText.gameObject.SetActive(true);
+            correctText.gameObject.SetActive(false);
+            correctText.text = "Correct answers:" + correctNum;
+            incorrectText.text = "Incorrect answers:" + incorrectNum;
+        }
+    } */
 }
